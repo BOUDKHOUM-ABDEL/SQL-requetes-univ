2b3c753c7140db5a236e8657df3a76b688ad1a6f
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-20 00:08:31 +0100

    update

--- vols-quotidiens-bd/2______insert--Q1.sql
+++ vols-quotidiens-bd/2______insert--Q1.sql
@@ -1,4 +1,34 @@
 
+----------------------Tableau Avion-----------------------------
+
+INSERT INTO Avion (NA, Nom, Capacite, Localite) VALUES
+(100, 'AIRBUS', 300, 'RABAT'),
+(101, 'B737', 220, 'CASA'),
+(102, 'A320', 180, 'FES'),
+(103, 'B777', 350, 'MARRAKECH'),
+(104, 'ATR72', 70, 'AGADIR');
+
+
+----------------------Tableau Pilote-----------------------------
+
+INSERT INTO Pilote (NP, Nom, Adresse) VALUES
+(1, 'Ali Benbrahim', '12 rue Hassan II, Rabat'),
+(2, 'Khadija Amrani', '33 bd Zerktouni, Casablanca'),
+(3, 'Youssef El Fassi', '7 lot Massira, Fès'),
+(4, 'Fatima Zahra', '23 avenue Mohammed V, Marrakech'),
+(5, 'Omar Idrissi', '10 rue Moulay Ismail, Agadir');
+
+
+----------------------Tableau vol-------------------------------
+
+INSERT INTO Vol (NV, NP, NA, VD, VA, HD, HA) VALUES
+('IT100', 1, 100, 'RABAT', 'CASA', 800, 900),
+('IT101', 2, 101, 'CASA', 'FES', 930, 1030),
+('IT102', 3, 102, 'FES', 'MARRAKECH', 1100, 1230),
+('IT103', 4, 103, 'MARRAKECH', 'AGADIR', 1400, 1530),
+('IT104', 2, 104, 'AGADIR', 'RABAT', 1600, 1800),
+('IT105', 5, 101, 'CASA', 'TANGER', 700, 830),
+('IT106', 2, 100, 'RABAT', 'OUJDA', 1000, 1130);
 
 
 
--- vols-quotidiens-bd/3______queries--Q(2-).sql
+++ vols-quotidiens-bd/3______queries--Q(2-).sql
@@ -18,13 +18,42 @@
 
    -- 6) Afficher les avions dans la localité est Rabat ou Casa:
 
+        SELECT * FROM Avion
+        WHERE Localite = 'RABAT' OR Localite = 'CASA';
 
-    7) Modifier la capacité de l’avion numéro 101, la nouvelle capacité et 220
-    8) Supprimer les avions dans la capacité et inférieure à 200
-    9) Afficher la capacité maximale, minimale, moyenne des avions
-    10) Afficher les données des avions dont la capacité et la plus basse
-    11) Afficher les données des avions dont la capacité et supérieure à la capacité moyenne
-    12) Afficher le nom et l’adresse des pilotes assurant les vols IT100 et IT104
-    13) Afficher les numéros des pilotes qui sont en service
-    14) Afficher les numéros des pilotes qui ne sont pas en service
-    15) Afficher les noms des pilotes qui conduisent un AIRBUS
+   -- 7) Modifier la capacité de l’avion numéro 101, la nouvelle capacité et 220:
+
+        UPDATE Avion
+        SET Capacite = 220
+        WHERE NA = 101;
+
+   -- 8) Supprimer les avions dans la capacité est inférieure à 200:
+
+        DELETE FROM Avion
+        WHERE Capacite <= 200;
+
+
+   -- 9) Afficher la capacité maximale, minimale, moyenne des avions:
+
+        SELECT 
+          MAX(Capacite) AS Capacite_Max,
+          MIN(Capacite) AS Capacite_Min,
+          AVG(Capacite) AS Capacite_Moyenne
+        FROM Avion;
+
+   -- 10) Afficher les données des avions dont la capacité est la plus basse:
+
+        SELECT * FROM Avion
+        WHERE Capacite = (SELECT MIN(Capacite) FROM Avion);
+
+   -- 11) Afficher les données des avions dont la capacité est supérieure à la capacité moyenne:
+
+        SELECT * FROM Avion
+        WHERE Capacite > (SELECT AVG(Capacite) FROM Avion);
+
+
+   -- 12) Afficher le nom et l’adresse des pilotes assurant les vols IT100 et IT104:
+
+   -- 13) Afficher les numéros des pilotes qui sont en service
+   -- 14) Afficher les numéros des pilotes qui ne sont pas en service
+   -- 15) Afficher les noms des pilotes qui conduisent un AIRBUS
