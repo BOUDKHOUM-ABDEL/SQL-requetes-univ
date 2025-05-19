89faf40d3e98c66aeb27bf3313c4ba23a10d0dd4
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-19 21:47:26 +0100

    Create insert--Q1.sql

---
+++ vols-quotidiens-bd/insert--Q1.sql
@@ -0,0 +1,4 @@
+/*1) Insérer les avions suivants dans la table Avion :
+     (100, AIRBUS, 300, RABAT), (101,B737,250,CASA), (102, B737,220,RABAT)*/
+
+INSERT INTO avion VALUES (100, 'AIRBUS', 300, 'RABAT'), (101,'B737',250,'CASA'), (102, 'B737',220,'RABAT');
