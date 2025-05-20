56d6ecc9cc810b9a33d2629049a48a923078402f
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-20 02:23:34 +0100

    update

--- gestion-projet-bd/1______schema.sql
+++ gestion-projet-bd/1______schema.sql
@@ -1,33 +1,42 @@
-Soit la base de données intitulée "gestion_projet" permettant de gérer les projets relatifs au 
-développement de logiciels. 
-Elle est décrite par la représentation textuelle simplifiée suivante : 
-- Developpeur (NumDev, NomDev, AdrDev, EmailDev, TelDev)
-- Projet (NumProj, TitreProj, DateDeb, DateFin)
+
 - Logiciel (CodLog, NomLog, PrixLog, #NumProj) - Realisation (#NumProj, #NumDev)
 
+-- Developpeur (NumDev, NomDev, AdrDev, EmailDev, TelDev)
+
+	create table Developpeur (
+		NumDev int primary key,
+		NomDev varchar(20),
+		AdrDev varchar(30), 
+		EmailDev varchar(30),
+		TelDev varchar(30)
+	);
 
 
-create table Developpeur (
-	NumDev int primary key,
-	NomDev varchar(20),
-	AdrDev varchar(30), 
-	EmailDev varchar(30),
-	TelDev varchar(30)
- 
-);
+-- Projet (NumProj, TitreProj, DateDeb, DateFin)
 
-create table Projet (
-	NumProj int primary key,
-	TitreProj varchar(15),
-	DateDeb varchar(10),
-	DateFin varchar(10)
-);
-- Logiciel (CodLog, NomLog, PrixLog, #NumProj) - Realisation (#NumProj, #NumDev)
-(1001, 'StockMaster', 15000, 10),
+	create table Projet (
+		NumProj int primary key,
+		TitreProj varchar(15),
+		DateDeb varchar(10),
+		DateFin varchar(10)
+	);
 
-create table Logiciel (
-	CodLog int primary key,
-    NomLog varchar(10),
-	PrixLog int, 
-	NumProj int,
-primary key (NumProj) REFERENCES Projet(NumProj) - Realisation (#NumProj, #NumDev)
+
+-- Logiciel (CodLog, NomLog, PrixLog, #NumProj) 
+
+	create table Logiciel (
+		CodLog int primary key,
+		NomLog varchar(10),
+		PrixLog int, 
+		NumProj int,
+	foriegn key (NumProj) REFERENCES Projet(NumProj) 
+	
+-- Realisation (#NumProj, #NumDev)
+	
+	CREATE TABLE Realisation (
+		NumProj INT,
+		NumDev INT,
+		PRIMARY KEY (NumProj, NumDev),
+		FOREIGN KEY (NumProj) REFERENCES Projet(NumProj),
+		FOREIGN KEY (NumDev) REFERENCES Developpeur(NumDev)
+	);
