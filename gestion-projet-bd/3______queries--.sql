f0ffe9630ce5638f83495100ca85aea1e8c1398e
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-26 23:41:45 +0100

    update

--- gestion-projet-bd/3______queries--.sql
+++ gestion-projet-bd/3______queries--.sql
@@ -4,3 +4,8 @@ SELECT NomLog, PrixLog
 FROM Logiciel
 WHERE NumProj = (SELECT NumProj FROM Projet WHERE TitreProj = 'gestion de stock')
 ORDER BY PrixLog DESC;
+
+-- 2. Total des prix des logiciels du projet numéro 10
+SELECT SUM(PrixLog) AS "coût total du projet"
+FROM Logiciel
+WHERE NumProj = 10;
