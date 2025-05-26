61350beb2295738e4edbb021ebace66091a7368d
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-26 23:43:58 +0100

    update

--- gestion-projet-bd/3______queries--.sql
+++ gestion-projet-bd/3______queries--.sql
@@ -9,3 +9,8 @@ ORDER BY PrixLog DESC;
 SELECT SUM(PrixLog) AS "coût total du projet"
 FROM Logiciel
 WHERE NumProj = 10;
+
+-- 3. Nombre de développeurs ayant participé au projet "gestion de stock"
+SELECT COUNT(DISTINCT NumDev) AS nb_developpeurs
+FROM Realisation
+WHERE NumProj = (SELECT NumProj FROM Projet WHERE TitreProj = 'gestion de stock');
